36e397df5ce01b7d5fbb13ec7784341a5b94d597
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2023-02-12 17:26:55 -0300

    moved the reset-password code to file reset.js

--- routes/api/login.js
+++ routes/api/login.js
@@ -5,7 +5,6 @@ const User = require('../../models/User');
 const { Router } = require('express');
 const router = express.Router();
 const passport = require('passport')
-const nodemailer = require('nodemailer');
 
 // GET /register
 router.get('/register', (req, res, next) => {
@@ -48,44 +47,6 @@ function isAuthenticated(req, res, next) {
 */
 
 
-/* send email to reset password */
-router.post('/resetpass',async (req, res, next) => {
-  // const email = await User.findOne(req.body.email)
-  console.log(req.body)
-  try {
-    // const email = req.body.email
-
-  // if(email) {
-    const emailContent = {
-      to: `${req.body.email}`,
-      subject: 'Reset your Flitter password',
-      html: `Hi ${req.body.name}, <br> Click the following link to reset yout password: <br>
-      http://localhost:3000/reset-password`
-    }
-    // const transport = nodemailer.createTransport({
-    //   host: process.env.SMTP_HOST,
-    //   port: process.env.SMTP_PORT,
-    //   auth: {
-    //     user: process.env.SMTP_USER,
-    //     pass: process.env.SMTP_PASS
-    //   }
-    // })
-    var transport = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: "6dda7148a9b54a",
-        pass: "14df1d0daa21ad"
-      }
-    });
-    await transport.sendMail(emailContent)
-    res.json({Message: 'message sent'})
-  // }
-  } catch (error) {
-    next(error)
-  }
-
-})
 
 module.exports = router;
   
