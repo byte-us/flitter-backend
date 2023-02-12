e36576a077ab623de4c9d051bc01e67d132130fa
Irene Aunon <irene@ireneaunion.com>
2023-02-12 12:22:17 +0100

    Revert "Revert "get register and login mostly working""
    This reverts commit e277c86e93a06b92fb8a06cc6a30965e5838f093.

--- passport/local-auth.js
+++ passport/local-auth.js
@@ -1,48 +1,54 @@
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const User = require('../models/User')
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+const crypto = require("crypto");
 
 passport.serializeUser((user, done) => {
-    done(null, user.id)
+  done(null, user.id);
 });
 
-passport.deserializeUser( async(id, done) => {
-    const user = await User.findById(id)
-    done(null, user)
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
 });
 
-
-passport.use('local-register', new localStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-} , async (req, email, password, done) => {
-
-    const user = await User.findOne({ email: email })
-    if (user) {
-        return done(null, false, req.flash('registerMessage', 'The email is already taken'))
-    } else {
-        const newUser = newUser();
-        newUser.email = email;
-        newUser.password = newUser.encryptPassword(password);
-        await newUser.save();
-        done(null, newUser);
+console.log("register local strategy");
+passport.use(
+  // Passport specifically expects a "username" parameter, which means we need to
+  // send the email as the username. This is not the same as our username.
+  new LocalStrategy(async function verify(username, password, cb) {
+    try {
+      const user = await User.findOne({ email: username });
+      if (!user) {
+        return cb(null, false, { message: "Incorrect username or password." });
+      }
+      crypto.pbkdf2(
+        password,
+        Buffer.from(user.salt, "hex"),
+        310000,
+        32,
+        "sha256",
+        function (err, hashedPassword) {
+          if (err) {
+            console.error(err);
+            return cb(err);
+          }
+          if (
+            !crypto.timingSafeEqual(
+              Buffer.from(user.password, "hex"),
+              hashedPassword
+            )
+          ) {
+            return cb(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          return cb(null, user);
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      cb(err);
     }
-}))
-
-passport.use('local-login', new localStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-
- 
-}, async (req, email, password, done) => {
-    User.findOne({ email: email })
-    if (!user) {
-        return done(null, false, req.flash('loginMessage', 'No user found'))
-    } if (user.comparePassword(password)) {
-        return done(null, false, req.flash('loginMessage', 'Incorrect Password'))
-    }
-    done(null, user)
-}));
+  })
+);
