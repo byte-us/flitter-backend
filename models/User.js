ce912680616aa51f2898f09eb7a922792d407a4d
Anastasia Sys <stasy.sys@mail.ru>
2023-02-13 04:51:16 +0100

    feat: Working auth

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt');
 const {Schema} = mongoose;
 
 const saltRounds = 10;
@@ -31,7 +31,22 @@ const userSchema = new mongoose.Schema({
     ]
 }, { timestamps: true });
 
+userSchema.pre('validate', function(next) {
+  if(this.isNew || this.isModified('password')) {
+    const document = this;
 
+    bcrypt.hash(document.password, saltRounds, (err, hashedPassword) => {
+      if(err) {
+        next(err);
+      } else {
+        document.password = hashedPassword;
+        next();
+      }
+    })
+  } else {
+    next();
+  }
+});
 
 userSchema.methods.encryptPassword = function(password) {
  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
